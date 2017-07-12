63f4566c903d46495e3a91838a834b811a749d32
Артем Моисеенко <artyom_moiseenko@mail.ru>
2017-07-12 11:10:20 +0300

    1 Add controller and views for work with favorite cities

---
+++ Homework/Controllers/FavoriteCitiesController.cs
@@ -0,0 +1,96 @@
+﻿using Homework.Database.DAL.UnitOfWork;
+using Homework.Database.Entities;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class FavoriteCitiesController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FavoriteCitiesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: FavoriteCities
+        public ActionResult Index()
+        {
+            return View(_unitOfWork.CityRepository.Get().ToList());
+        }
+
+        // GET: FavoriteCities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FavoriteCities/Create
+        [HttpPost]
+        public ActionResult Create(City city)
+        {
+            try
+            {
+                _unitOfWork.CityRepository.Create(city);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(city);
+            }
+        }
+
+        // GET: FavoriteCities/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(_unitOfWork.CityRepository.FindById(id));
+        }
+
+        // POST: FavoriteCities/Edit/5
+        [HttpPost]
+        public ActionResult Edit(City city)
+        {
+            try
+            {
+                _unitOfWork.CityRepository.Update(city);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(city);
+            }
+        }
+
+        // GET: FavoriteCities/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(_unitOfWork.CityRepository.FindById(id));
+        }
+
+        // POST: FavoriteCities/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            try
+            {
+                _unitOfWork.CityRepository.Remove(_unitOfWork.CityRepository.FindById(id));
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- Homework/Services/WeatherService.cs
+++ Homework/Services/WeatherService.cs
@@ -15,21 +15,23 @@ namespace Homework.Services
     public class WeatherService : IWeatherService
     {
         private readonly string _apiKey;
+        private readonly string _baseUrl;
 
         public WeatherService()
         {
             _apiKey = WebConfigurationManager.AppSettings["ApiKey"];
+            _baseUrl = WebConfigurationManager.AppSettings["BaseUrl"];
         }
 
         public async Task<WeatherModel> GetWeatherCity(string city, string countDays)
         {
-            var url = $"http://api.openweathermap.org/data/2.5/forecast/daily?q={city}&units=metric&cnt={countDays}&appid={_apiKey}";
+            var url = $"{_baseUrl}/data/2.5/forecast/daily?q={city}&units=metric&cnt={countDays}&appid={_apiKey}";
             var client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync(url);
             var json = await response.Content.ReadAsStringAsync();
             var weather = JsonConvert.DeserializeObject<WeatherModel>(json);
             var nameIcon = weather.List.FirstOrDefault().Weather.FirstOrDefault().Icon;
-            weather.List.FirstOrDefault().Weather.FirstOrDefault().Icon = $"http://openweathermap.org/img/w/{nameIcon}.png";
+            weather.List.FirstOrDefault().Weather.FirstOrDefault().Icon = $"{_baseUrl}/img/w/{nameIcon}.png";
 
             return weather;
         }
